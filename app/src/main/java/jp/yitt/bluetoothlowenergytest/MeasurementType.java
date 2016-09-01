d2626d0a987146112007f2de6b1f769e6223b063
今田 大聖 <genm1023@gmail.com>
2016-09-01 16:53:23 +0900

    MeasurementType追加

---
+++ app/src/main/java/jp/yitt/bluetoothlowenergytest/MeasurementType.java
@@ -0,0 +1,13 @@
+package jp.yitt.bluetoothlowenergytest;
+
+/**
+ * Created by genm1023 on 9/1/16.
+ */
+public class MeasurementType {
+    public enum Type{
+        LENGTH,
+        AREA,
+        CUBE
+    }
+
+}

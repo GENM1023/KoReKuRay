9a2e5046a71d13f48cbc045af00be9b1b0bbdc8b
今田 大聖 <genm1023@gmail.com>
2016-09-02 11:20:16 +0900

    cardview追加など

--- app/src/main/java/jp/yitt/bluetoothlowenergytest/SuggestFragment.java
+++ app/src/main/java/jp/yitt/bluetoothlowenergytest/SuggestFragment.java
@@ -1,11 +1,26 @@
 package jp.yitt.bluetoothlowenergytest;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 /**
  * Created by genm1023 on 9/2/16.
  */
 public class SuggestFragment extends Fragment {
+    
+    public SuggestFragment() {
+    }
+
+    public static SuggestFragment newInstance(){
+        return new SuggestFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+
+    }
 
     @Override
     public void onPause() {

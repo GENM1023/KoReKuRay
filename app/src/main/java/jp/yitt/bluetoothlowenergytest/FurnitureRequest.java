a4a56846b2cf6a811b453a0359840aef5375f919
鎗田浩平 <yaritakouhei@yaritakouhei-no-MacBook-Air.local>
2016-10-21 16:24:37 +0900

    リストのデータを遷移先が取得

---
+++ app/src/main/java/jp/yitt/bluetoothlowenergytest/FurnitureRequest.java
@@ -0,0 +1,15 @@
+package jp.yitt.bluetoothlowenergytest;
+
+/**
+ * Created by yaritakouhei on 2016/10/21.
+ */
+public class FurnitureRequest {
+    final String name;
+    final String length;
+
+    FurnitureRequest(String name, String length) {
+        this.name = name;
+        this.length = length;
+    }
+
+}
